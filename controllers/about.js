dc84606c49997737c4ac05d2746c8d6f259b2bde
sorawat38 <st.sorawat38@gmail.com>
2024-10-13 03:24:05 -0600

    [feat] add about us

--- controllers/about.js
+++ controllers/about.js
@@ -2,7 +2,8 @@ const Employee = require("../models/employee");
 
 const renderAboutPage = async (req, res) => {
   try {
-    res.render("about");
+    const employees = await Employee.findAll();
+    res.render("about", { employees });
   } catch (error) {
     console.error(error); // TODO: handle error later
   }
