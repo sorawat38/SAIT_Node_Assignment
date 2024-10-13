f56dd212e72e027e0870d239c1ac56d9b0c274c6
sorawat38 <st.sorawat38@gmail.com>
2024-10-13 02:24:44 -0600

    [feat] add employee table

---
+++ models/employee.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/db");
+
+const Employee = sequelize.define("employee", {
+  id: {
+    type: DataTypes.UUIDV4,
+    primaryKey: true,
+    allowNull: false,
+  },
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  position: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  photo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  bio: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Employee;
